0ae7cda5f9bc09aee555ecc70d7681fab615b35e
anamunlam <anam@unlam.ac.id>
2017-08-15 08:57:04 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OS=$( lsb_release -si | tr [A-Z] [a-z] )
+CODENAME=$( lsb_release -sc )
+if [[ "$OS" != "debian" ]]; then
+  echo "OS not supported, please do manual install"
+  exit 1
+fi
+
+INSTALL=$(wget -qO - "https://github.com/anamunlam/ahkacp/raw/master/debian/${CODENAME}.sh")
+if [ "$?" -eq '0' ]; then
+  bash -c "${INSTALL}"
+fi
+exit
