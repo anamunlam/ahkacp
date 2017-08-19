740d05f6516af16726456c00ed858612ab2aacab
anamunlam <anam@unlam.ac.id>
2017-08-19 14:34:09 +0800

    Delete configs.php

--- includes/configs.php
+++
@@ -1,9 +0,0 @@
-<?php
-$configs = array(
-    'name'=>'AHKACP',
-    'desc'=>'Control Panel',
-    'theme'=>'default'
-);
-
-define('AHKA', '/usr/local/ahkacp');
-define('AHKA_CMD', 'sudo /bin/bash '.AHKA.'/bin/');
